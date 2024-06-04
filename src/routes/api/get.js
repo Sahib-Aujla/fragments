362d09d9e2058f9e653808c3b6382b10c23ea5be
Sahib.Aujla <sahibpreet568@gmail.com>
2024-06-04 00:05:41 -0400

    added logic to send back fragment

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,11 +1,15 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require("../../response");
+const { createSuccessResponse } = require('../../response');
 
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-    // TODO: this is just a placeholder. To get something working, return an empty array...
-    res.status(200).json(createSuccessResponse( {fragments:[]}));
-  };
+module.exports = async (req, res) => {
+  // TODO: this is just a placeholder. To get something working, return an empty array...
+  const data = await Fragment.byUser(req.user,true);
+  logger.debug({ data });
+  res.status(200).json(createSuccessResponse({ fragments: data }));
+};
