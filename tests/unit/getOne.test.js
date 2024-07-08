ce82302a5450f59e74c79aaba984864851f89df9
Sahib.Aujla <sahibpreet568@gmail.com>
2024-07-08 17:35:59 -0400

    added test cases for getOne endpoint with extension

--- tests/unit/getOne.test.js
+++ tests/unit/getOne.test.js
@@ -34,6 +34,37 @@ describe('GET /v1/fragments/:id', () => {
     expect(res.text).toEqual('hello');
   });
 
+  test('authenticated user gets a converted fragment from markdown to html', async () => {
+    const res1 = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Hello');
+
+    logger.debug({ res1 });
+    const res = await request(app)
+      .get(`/v1/fragments/${res1.body.fragment.id}.html`)
+      .auth('user1@email.com', 'password1');
+    logger.debug({ res });
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toEqual('<h1>Hello</h1>\n');
+  });
+
+  test('authenticated user gets a converted fragment from html to text', async () => {
+    const res1 = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/html')
+      .send('<h1>Hello</h1>');
+
+    logger.debug({ res1 });
+    const res = await request(app)
+      .get(`/v1/fragments/${res1.body.fragment.id}.txt`)
+      .auth('user1@email.com', 'password1');
+    logger.debug({ res });
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toEqual('<h1>Hello</h1>');
+  });
   test('authenticated users sends an invalid id', async () => {
     const res = await request(app).get(`/v1/fragments/abc`).auth('user1@email.com', 'password1');
     logger.debug({ res });
